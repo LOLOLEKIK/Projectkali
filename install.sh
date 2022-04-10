34bf0c5e72b2b40995518914cf287a7d384afa90
Loris THIEBAUT <lololekik@pop-os.localdomain>
2022-04-10 18:50:36 +0200

    correction
    correction du test png
    
    Signed-off-by: Loris THIEBAUT <lololekik@pop-os.localdomain>

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo cp .bashrc.new /usr/share/kaliproject
 sudo cp Dockerfile /usr/share/kaliproject/
 sudo cp history /usr/share/kaliproject/
 #test
-if [ -e /usr/bin/kali -a -e /usr/share/kaliproject/pentest.png -a -e /usr/share/kaliproject/Dockerfile -a -e /share/kali -a -e /usr/share/kaliproject/history ]
+if [ -e /usr/bin/kali -a -e /share/kali -a -e /usr/share/kaliproject/Dockerfile -a -e /usr/share/kaliproject/history ]
 then
     echo "L'installation semble avoir fonctionné"
 else
